233caad6185c6af80cb9d1a83d3e430abd2dc025
Kaori <74669798+ogipote@users.noreply.github.com>
2022-08-21 06:05:29 +0300

    Update index.js

--- website/index.js
+++ website/index.js
@@ -12,10 +12,10 @@ app.use('/api', apiRouter);
 
 app.get('/docs', (req, res) => res.redirect('https://github.com/HordLawk/YottaBot#get-started'));
 
-app.get('/topgg', (req, res) => res.redirect('https://top.gg/bot/371902120561082368'));
+app.get('/topgg', (req, res) => res.redirect('https://top.gg/fr/bot/855107430693077033'));
 
 app.get('/privacy', (req, res) => res.redirect('https://github.com/HordLawk/YottaBot/wiki/Privacy-Policy'));
 
-app.get('*', (req, res) => res.redirect(`https://discord.com/api/oauth2/authorize?client_id=371902120561082368&permissions=${permissions}&scope=bot+applications.commands`));
+app.get('*', (req, res) => res.redirect(`https://discord.com/api/oauth2/authorize?client_id=983162005009600512&permissions=${permissions}&scope=bot+applications.commands`));
 
 app.listen(process.env.PORT || port, () => console.log(`Server listening at port ${process.env.PORT || port}`));
